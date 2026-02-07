ff322bde58e533a5d5cbac06e2a6dddc361eb3e2
xroldan <pablo@gmail.com>
2026-02-07 02:22:31 +0100

    Added m3u

--- scripts/update_and_push.sh
+++ scripts/update_and_push.sh
@@ -3,15 +3,22 @@
 # Navegar al directorio del repositorio
 cd /home/compartir/lista_zeronet/enlaces-ace/
 
+# Copiar el archivo local para que GitHub Pages pueda leerlo (evitando error de symlink)
+# Aseguramos que el archivo existe antes de copiarlo
+if [ -f "/home/compartir/local.m3u" ]; then
+    cp /home/compartir/local.m3u data/local.m3u
+fi
+
 # Ejecutar el script de actualización
 /usr/bin/python3 actualizar.py
 
-# Verificar si hay cambios en el archivo de canales
-if git status --porcelain data/canales.json | grep -q "^ M"; then
-    echo "Cambios detectados en los canales. Subiendo a GitHub..."
-    git add data/canales.json
-    git commit -m "Auto-update channels $(date +'%Y-%m-%d %H:%M:%S')"
+# Verificar si hay cambios en el archivo de canales o en la lista de eventos
+if git status --porcelain data/canales.json data/local.m3u | grep -q "^ M\|^??"; then
+    echo "Cambios detectados. Subiendo a GitHub..."
+    git add data/canales.json data/local.m3u
+    git commit -m "Auto-update channels and events $(date +'%Y-%m-%d %H:%M:%S')"
+    # Forzamos IdentitiesOnly por si acaso
     git push origin main
 else
-    echo "No hay cambios nuevos en los canales. Todo al día."
+    echo "No hay cambios nuevos. Todo al día."
 fi
